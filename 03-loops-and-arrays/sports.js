cc18971c4903637cefd70775405cc4e4d13a0b01
Hikmet Suiçmez <suicmezhikmet1@gmail.com>
2023-02-09 16:49:58 +0300

    typescript -> loops - arrays

---
+++ 03-loops-and-arrays/sports.js
@@ -0,0 +1,10 @@
+var sportsOne = ["Golf", "Cricket", "Tennis", "Swimming"];
+for (var _i = 0, sportsOne_1 = sportsOne; _i < sportsOne_1.length; _i++) {
+    var template = sportsOne_1[_i];
+    if (template == "Cricket") {
+        console.log(template + " << My Favorite!");
+    }
+    else {
+        console.log(template);
+    }
+}
---
+++ 03-loops-and-arrays/sports.ts
@@ -0,0 +1,11 @@
+let sportsOne: string[] = ["Golf","Cricket", "Tennis", "Swimming"];
+
+for (let template of sportsOne) {
+
+    if (template == "Cricket") {
+        console.log(template + " << My Favorite!");
+
+    } else {
+        console.log(template);
+    }
+}
